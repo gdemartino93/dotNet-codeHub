218123aeb578e201460fcda0afb1d82c3bbba467
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-21 22:31:31 +0200

    form validation

--- dotnet-codeHub/Models/Category.cs
+++ dotnet-codeHub/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace dotnet_codeHub.Models
@@ -7,9 +8,12 @@ namespace dotnet_codeHub.Models
     {
         [Key]
         public int Id { get; set; }
+        [MinLength(1,ErrorMessage = "Il nome non può essere vuoto")]
+        [MaxLength(15,ErrorMessage = "Il nome non può superare 15 caratteri")]
         public string Name { get; set; }
         public string? Description { get; set; }
         [DisplayName("Ordine visualizzazione")]
+        [Range(1,15,ErrorMessage = "Deve essere un numero da 1 a 15")]
         public int DisplayOrder { get; set; }
         public bool IsVisible { get; set; }
 
