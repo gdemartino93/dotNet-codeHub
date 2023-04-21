38552d7466da458c9d7cbe0da592d8094dc7e1f1
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-21 21:36:49 +0200

    db seed

--- dotnet-codeHub/Data/ApplicationDbContext.cs
+++ dotnet-codeHub/Data/ApplicationDbContext.cs
@@ -10,5 +10,14 @@ namespace dotnet_codeHub.Data
         {
             
         }
+        //seeding database
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category() { Id = 1, Name = "Java", Description = "Java World", DisplayOrder = 1, IsVisible = true },
+                new Category() { Id = 2, Name = "C#", Description = "C# World", DisplayOrder = 1, IsVisible = true },
+                new Category() { Id = 3, Name = "CSS", Description = "CSS World", DisplayOrder = 1, IsVisible = true }
+                );
+        }
     }
 }
