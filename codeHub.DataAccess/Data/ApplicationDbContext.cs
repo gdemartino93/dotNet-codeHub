293adccda08b4585fcc6883fcac8e9ac8d98b18e
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-23 22:19:07 +0200

    create model for course

--- codeHub.DataAccess/Data/ApplicationDbContext.cs
+++ codeHub.DataAccess/Data/ApplicationDbContext.cs
@@ -6,6 +6,8 @@ namespace codeHub.DataAccess.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
             
@@ -18,6 +20,59 @@ namespace codeHub.DataAccess.Data
                 new Category() { Id = 2, Name = "C#", Description = "C# World", DisplayOrder = 1, IsVisible = true },
                 new Category() { Id = 3, Name = "CSS", Description = "CSS World", DisplayOrder = 1, IsVisible = true }
                 );
+            modelBuilder.Entity<Course>().HasData(
+                new Course()
+                {
+                    Id = 1,
+                    Author = "John Doe",
+                    CreatedAt = new DateTime(2022, 1, 10),
+                    Description = "Learn the fundamentals of computer science with Python",
+                    LastUpdatedAt = null,
+                    Level = "Intermediate",
+                    Price = 50,
+                    Text = "This course is designed to teach you the basics of computer science using Python. Topics include: algorithms, data structures, recursion, sorting, and searching. By the end of the course, you will have a strong foundation in computer science and be able to write basic programs in Python.",
+                    Title = "Introduction to Computer Science with Python"
+                },
+
+                new Course()
+                {
+                    Id = 2,
+                    Author = "Jane Smith",
+                    CreatedAt = new DateTime(2022, 2, 20),
+                    Description = "Learn how to build scalable and performant web applications with React",
+                    LastUpdatedAt = new DateTime(2022, 3, 5),
+                    Level = "Advanced",
+                    Price = 100,
+                    Text = "In this course, you will learn how to build modern web applications with React. Topics include: React components, state management with Redux, server-side rendering, and performance optimization. By the end of the course, you will be able to build scalable and performant web applications with React.",
+                    Title = "Mastering React"
+                },
+
+                new Course()
+                {
+                    Id = 3,
+                    Author = "Lucia Rossi",
+                    CreatedAt = new DateTime(2022, 4, 1),
+                    Description = "Learn how to build full-stack web applications with Node.js and MongoDB",
+                    LastUpdatedAt = null,
+                    Level = "Intermediate",
+                    Price = 75,
+                    Text = "This course teaches you how to build full-stack web applications using Node.js and MongoDB. Topics include: RESTful APIs, authentication and authorization, database design, and deployment. By the end of the course, you will be able to build and deploy a full-stack web application with Node.js and MongoDB.",
+                    Title = "Full-Stack Web Development with Node.js and MongoDB"
+                },
+
+                new Course()
+                {
+                    Id = 4,
+                    Author = "Mario Bianchi",
+                    CreatedAt = new DateTime(2022, 3, 15),
+                    Description = "Learn how to use machine learning to analyze and visualize data",
+                    LastUpdatedAt = new DateTime(2022, 4, 10),
+                    Level = "Advanced",
+                    Price = 150,
+                    Text = "In this course, you will learn how to use machine learning techniques to analyze and visualize data. Topics include: data preprocessing, dimensionality reduction, clustering, classification, and regression. You will also learn how to use popular machine learning libraries such as scikit-learn and TensorFlow. By the end of the course, you will be able to apply machine learning techniques to real-world data analysis and visualization problems.",
+                    Title = "Machine Learning for Data Analysis and Visualization"
+                }
+            );
         }
     }
 }
---
+++ codeHub.Models/Course.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codeHub.Models
+{
+    public class Course
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string Text { get; set; }
+        public string? Level { get; set; }
+        public double Price { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? LastUpdatedAt { get; set; }
+
+
+    }
+}
