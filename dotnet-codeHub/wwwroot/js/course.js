5f58ad6cecedfe2b2847e230b1def93fedccc09c
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-24 22:21:51 +0200

    imported ajax table

---
+++ dotnet-codeHub/wwwroot/js/course.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/course/getall' },
+        "columns": [
+            { data: 'title', "width": "10%" },
+            { data: 'description', "width": "10%" },
+            { data: 'price', "width": "10%" },
+            { data: 'category.name', "width": "10%" },
+            { data: 'createdAt', "width": "15%" },
+        
+           
+        ]
+    });
+}
