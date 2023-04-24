010c633ae5c87ff64c5df4f5aad4b435b7e0cdfa
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-24 22:49:41 +0200

    style and btn in the table

--- dotnet-codeHub/wwwroot/js/course.js
+++ dotnet-codeHub/wwwroot/js/course.js
@@ -7,10 +7,18 @@ function loadDataTable() {
         "ajax": { url: '/admin/course/getall' },
         "columns": [
             { data: 'title', "width": "10%" },
-            { data: 'description', "width": "10%" },
+            { data: 'description', "width": "1%" },
             { data: 'price', "width": "10%" },
             { data: 'category.name', "width": "10%" },
-            { data: 'createdAt', "width": "15%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group btnTable" role="group">
+                     <a href="/admin/course/upsert?id=${data}" class="btn btn-primary"> <i class="bi bi-pencil-square"></i> Edit</a>               
+                     <a href="/admin/course/delete/${data}") class="btn btn-danger"> <i class="bi bi-trash-fill"></i> Delete</a>
+                    </div>`
+                }
+            }
         
            
         ]
