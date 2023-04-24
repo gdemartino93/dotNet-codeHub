38e13b23bbcd86a4624ffc03a09463528c263528
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-25 00:57:11 +0200

    show price if course isnt free

--- dotnet-codeHub/wwwroot/js/course.js
+++ dotnet-codeHub/wwwroot/js/course.js
@@ -6,14 +6,14 @@ function loadDataTable() {
     dataTable = $('#tblData').DataTable({
         "ajax": { url: '/admin/course/getall' },
         "columns": [
-            { data: 'title', "width": "10%" },
-            { data: 'description', "width": "1%" },
+            { data: 'title', "width": "30%" },
+            { data: 'description', "width": "30%" },
             { data: 'price', "width": "10%" },
             { data: 'category.name', "width": "10%" },
             {
                 data: 'id',
                 "render": function (data) {
-                    return `<div class="w-75 btn-group btnTable" role="group">
+                    return `<div class="w-25 btn-group btnTable" role="group">
                      <a href="/admin/course/upsert?id=${data}" class="btn btn-primary"> <i class="bi bi-pencil-square"></i> Edit</a>               
                      <a href="/admin/course/delete/${data}") class="btn btn-danger"> <i class="bi bi-trash-fill"></i> Delete</a>
                     </div>`
